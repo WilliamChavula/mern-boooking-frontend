58ebd1fb84442f5bb10c94d26830a79401684bbf
williamchavula <rumbani@gmail.com>
2025-04-29 12:51:10 -0500

    install shadcn ui library

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         <span className="flex space-x-2">
           <Link
             to="/sign-in"
-            className="flex items-center bg-white text-blue-600 px-3 font-bold hover:bg-gray-100"
+            className="flex items-center rounded-sm bg-white text-blue-600 px-3 font-bold hover:bg-gray-100"
           >
             Sign in
           </Link>
