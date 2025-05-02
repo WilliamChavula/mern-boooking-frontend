b55eb6a5bb2562bf7e0c2d739d0ef0a310cebd84
williamchavula <rumbani@gmail.com>
2025-05-02 13:41:15 -0500

    prepare formData for sending to backend api

--- src/components/form/ManageHotelForm/ManageHotelForm.tsx
+++ src/components/form/ManageHotelForm/ManageHotelForm.tsx
@@ -7,18 +7,57 @@ import DetailsSection from "@/components/form/ManageHotelForm/DetailsSection.tsx
 import HotelTypesSection from "@/components/form/ManageHotelForm/HotelTypesSection.tsx";
 import FacilitiesSection from "@/components/form/ManageHotelForm/FacilitiesSection.tsx";
 import GuestsSection from "@/components/form/ManageHotelForm/GuestsSection.tsx";
+import ImagesSection from "@/components/form/ManageHotelForm/ImagesSection.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
 const ManageHotelForm = () => {
   const form = useForm<CreateHotelSchema>({
     resolver: zodResolver(createHotelSchema),
+    defaultValues: {
+      facilities: [],
+    },
   });
+
+  const onHotelFormSubmit = (jsonData: CreateHotelSchema) => {
+    const formData = new FormData();
+    formData.append("name", jsonData.name);
+    formData.append("city", jsonData.city);
+    formData.append("country", jsonData.country);
+    formData.append("description", jsonData.description);
+    formData.append("type", jsonData.type);
+    formData.append("adultCount", jsonData.adultCount.toString());
+    formData.append("childCount", jsonData.childCount.toString());
+    formData.append("pricePerNight", jsonData.pricePerNight.toString());
+    formData.append("starRating", jsonData.starRating.toString());
+
+    Array.from(jsonData.imageFiles).forEach((imageFile) => {
+      formData.append("imageFiles", imageFile);
+    });
+
+    jsonData.facilities.forEach((facility, index) => {
+      formData.append(`facilities[${index}]`, facility);
+    });
+  };
+
   return (
     <Form {...form}>
-      <form className="flex flex-col gap-5">
+      <form
+        className="flex flex-col gap-5"
+        onSubmit={form.handleSubmit(onHotelFormSubmit)}
+      >
         <DetailsSection />
         <HotelTypesSection />
         <FacilitiesSection />
         <GuestsSection />
+        <ImagesSection />
+        <span className="flex justify-end">
+          <Button
+            type="submit"
+            className="cursor-pointer bg-blue-600 hover:bg-blue-500 rounded-none text-white p-2 font-bold txxt-sm md:text-xl"
+          >
+            Save
+          </Button>
+        </span>
       </form>
     </Form>
   );
