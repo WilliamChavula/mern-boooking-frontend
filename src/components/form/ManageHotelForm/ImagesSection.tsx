6d31912e03f0cd0f9addf597fec228a5ef4f3bee
williamchavula <rumbani@gmail.com>
2025-05-02 13:20:14 -0500

    add ImagesSection

---
+++ src/components/form/ManageHotelForm/ImagesSection.tsx
@@ -0,0 +1,56 @@
+import { useFormContext } from "react-hook-form";
+
+import { CreateHotelSchema } from "@/types.ts";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form.tsx";
+import { Input } from "@/components/ui/input.tsx";
+
+const ImagesSection = () => {
+  const { control, getValues } = useFormContext<CreateHotelSchema>();
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-3">Images</h2>
+      <div className="rounded flex flex-col border border-gray-200">
+        <FormField
+          name="imageFiles"
+          control={control}
+          render={({ field }) => {
+            return (
+              <FormItem className="w-full py-5">
+                <FormControl className="w-full text-gray-700 font-normal">
+                  <div className="grid w-fit border-none max-w-sm items-center gap-1.5">
+                    <Input
+                      type="file"
+                      multiple
+                      accept="image/*"
+                      onChange={(evt) => {
+                        const files = evt.target.files;
+                        console.log("Raw files:", files);
+                        if (files) {
+                          field.onChange(files);
+                          console.log("Form value:", getValues("imageFiles")); // Use getValues
+                        }
+                      }}
+                      onBlur={field.onBlur}
+                      name={field.name}
+                      ref={field.ref}
+                      className="rounded-none shadow-none border-transparent file:border-0 file:px-4 file:bg-gray-200"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ImagesSection;
