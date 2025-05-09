211b1d03566a4cda1561b70c17e128d52c41295f
williamchavula <rumbani@gmail.com>
2025-05-08 22:24:22 -0500

    add favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,12 @@ const Header = () => {
     <div className="bg-blue-800 py-6 w-full">
       <div className="container px-5 md:px-0 mx-auto flex justify-between">
         <span className="text-lg md:text-3xl text-white font-bold tracking-tight">
-          <Link to="/">MernHolidays.com</Link>
+          <Link
+            to="/"
+            className="text-xl md:text-2xl lg:text-3xl tracking-tight"
+          >
+            bookingholidays.com
+          </Link>
         </span>
         {isLoggedIn ? (
           <div className="flex gap-5">
