b33d119b1900fd0563dcac0b23628f121146506c
williamchavula <rumbani@gmail.com>
2025-05-08 22:32:08 -0500

    amend name in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="bg-blue-800 py-10">
       <div className="container mx-auto flex flex-col md:flex-row gap-5 justify-between items-center">
         <span className="text-lg md:text-3xl text-white font-bold tracking-tight">
-          MernHolidays.com
+          bookingholidays.com
         </span>
         <span className="text-white text-xs tracking-tight flex gap-4">
           <p className="cursor-pointer">Privacy Policy</p>
