15f57e2b4bafca2d303a260260f6d524084bd101
william chavula <rumbani@gmail.com>
2025-11-24 22:13:44 -0600

    refactor: standardize import statements and improve formatting in LatestDestinationCard and Home components

--- src/components/LatestDestinationCard.tsx
+++ src/components/LatestDestinationCard.tsx
@@ -1,34 +1,34 @@
-import { BookingTypeSchema, HotelData } from "@/types.ts";
-import { Card, CardContent } from "@/components/ui/card.tsx";
-import { Link } from "react-router";
+import { HotelData } from '@/types.ts';
+import { Card, CardContent } from '@/components/ui/card.tsx';
+import { Link } from 'react-router';
 
 type LatestDestinationCardProps = {
-  hotel: HotelData & {
-    bookings: BookingTypeSchema[];
-  };
+    hotel: HotelData;
 };
 
 const LatestDestinationCard = ({ hotel }: LatestDestinationCardProps) => (
-  <Link
-    to={`/detail/${hotel._id}`}
-    className="relative cursor-pointer overflow-hidden rounded-none"
-  >
-    <Card className="h-[300px] py-0 rounded-none shadow-xs">
-      <img
-        src={
-          Array.isArray(hotel.imageUrls) ? hotel.imageUrls[0] : hotel.imageUrls
-        }
-        alt={`${hotel.name}-image`}
-        className="w-full h-full object-cover object-center"
-      />
+    <Link
+        to={`/detail/${hotel._id}`}
+        className='relative cursor-pointer overflow-hidden rounded-none'
+    >
+        <Card className='h-[300px] py-0 rounded-none shadow-xs'>
+            <img
+                src={
+                    Array.isArray(hotel.imageUrls)
+                        ? hotel.imageUrls[0]
+                        : hotel.imageUrls
+                }
+                alt={`${hotel.name}-image`}
+                className='w-full h-full object-cover object-center'
+            />
 
-      <CardContent className="absolute bottom-0 p-4 bg-black/50 w-full">
-        <p className="tracking-tight font-semibold text-white text-sm md:text-base">
-          {hotel.name}
-        </p>
-      </CardContent>
-    </Card>
-  </Link>
+            <CardContent className='absolute bottom-0 p-4 bg-black/50 w-full'>
+                <p className='tracking-tight font-semibold text-white text-sm md:text-base'>
+                    {hotel.name}
+                </p>
+            </CardContent>
+        </Card>
+    </Link>
 );
 
 export default LatestDestinationCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,55 +1,54 @@
-import { useGetAllHotels } from "@/api/hotel.api.ts";
-import { Loader, TriangleAlert } from "lucide-react";
-import LatestDestinationCard from "@/components/LatestDestinationCard.tsx";
+import { useGetAllHotels } from '@/api/hotel.api.ts';
+import { Loader, TriangleAlert } from 'lucide-react';
+import LatestDestinationCard from '@/components/LatestDestinationCard.tsx';
 
 const Home = () => {
-  const { hotels, isLoading } = useGetAllHotels();
+    const { hotels, isLoading } = useGetAllHotels();
 
-  if (isLoading) {
+    if (isLoading) {
+        return (
+            <div className='min-h-1/2 flex items-center justify-center'>
+                <Loader className='mr-2 animate-spin' /> Loading...
+            </div>
+        );
+    }
+
+    if (!hotels || !hotels.success) {
+        return (
+            <div className='min-h-1/2 flex flex-col md:flex-row items-center justify-center border border-gray-200 gap-4 p-4'>
+                <TriangleAlert size={32} className='text-orange-400' />
+                <p className='text-md md:text-lg text-gray-800 font-semibold'>
+                    No Hotels Found
+                </p>
+            </div>
+        );
+    }
+
+    const topRow = hotels.data.slice(0, 2);
+    const bottomRow = hotels.data.slice(2);
+
     return (
-      <div className="min-h-1/2 flex items-center justify-center">
-        <Loader className="mr-2 animate-spin" /> Loading...
-      </div>
+        <main className='space-y-3 mx-auto w-full lg:max-w-7xl'>
+            <h1 className='text-sm md:text-base lg:text-xl font-semibold'>
+                Latest Destinations
+            </h1>
+            <p className='text-xs md:text-sm'>
+                Most recent destinations added by our hosts
+            </p>
+            <section className='grid gap-4'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                    {topRow.map(hotel => (
+                        <LatestDestinationCard hotel={hotel} key={hotel._id} />
+                    ))}
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+                    {bottomRow.map(hotel => (
+                        <LatestDestinationCard hotel={hotel} key={hotel._id} />
+                    ))}
+                </div>
+            </section>
+        </main>
     );
-  }
-
-  if (!hotels || !hotels.success) {
-    return (
-      <div className="min-h-1/2 flex flex-col md:flex-row items-center justify-center border border-gray-200 gap-4 p-4">
-        <TriangleAlert size={32} className="text-orange-400" />
-        <p className="text-md md:text-lg text-gray-800 font-semibold">
-          No Hotels Found
-        </p>
-      </div>
-    );
-  }
-
-  const topRow = hotels.data.slice(0, 2);
-  const bottomRow = hotels.data.slice(2);
-
-
-  return (
-    <main className="space-y-3 mx-auto w-full lg:max-w-7xl">
-      <h1 className="text-sm md:text-base lg:text-xl font-semibold">
-        Latest Destinations
-      </h1>
-      <p className="text-xs md:text-sm">
-        Most recent destinations added by our hosts
-      </p>
-      <section className="grid gap-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {topRow.map((hotel) => (
-            <LatestDestinationCard hotel={hotel} key={hotel._id} />
-          ))}
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {bottomRow.map((hotel) => (
-            <LatestDestinationCard hotel={hotel} key={hotel._id} />
-          ))}
-        </div>
-      </section>
-    </main>
-  );
 };
 
 export default Home;
